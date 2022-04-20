16fbe4048291d346863f44c3333a1e563dd4bee7
Giannis Chatziveroglou <gchatz@mit.edu>
2022-04-20 17:33:01 -0400

    Fix reloading issue

--- components/useAddressName.tsx
+++ components/useAddressName.tsx
@@ -1,14 +1,14 @@
-import { tryGetName } from "@cardinal/namespaces";
+import { tryGetName } from '@cardinal/namespaces'
 import {
   formatShortAddress,
   useWalletIdentity,
-} from "@cardinal/namespaces-components";
-import styled from "@emotion/styled";
-import type { PublicKey } from "@solana/web3.js";
-import { useEnvironmentCtx } from "providers/EnvironmentProvider";
-import { useEffect, useState } from "react";
+} from '@cardinal/namespaces-components'
+import styled from '@emotion/styled'
+import type { PublicKey } from '@solana/web3.js'
+import { useEnvironmentCtx } from 'providers/EnvironmentProvider'
+import { useEffect, useState } from 'react'
 
-import { ContentLoader } from "./ContentLoader";
+import { ContentLoader } from './ContentLoader'
 
 const StyledLink = styled.div`
   margin: 0px auto;
@@ -20,9 +20,9 @@ const StyledLink = styled.div`
       opacity: 0.6;
     }
   }
-`;
+`
 export const formatTwitterLink = (handle: string | undefined) => {
-  if (!handle) return <></>;
+  if (!handle) return <></>
   return (
     <StyledLink>
       <a
@@ -33,49 +33,49 @@ export const formatTwitterLink = (handle: string | undefined) => {
         {handle}
       </a>
     </StyledLink>
-  );
-};
+  )
+}
 
 export const useAddressName = (
   address: PublicKey | undefined
 ): { displayName: string | undefined; loadingName: boolean } => {
-  const { connection } = useEnvironmentCtx();
-  const { handle } = useWalletIdentity();
-  const [displayName, setDisplayName] = useState<string | undefined>();
-  const [loadingName, setLoadingName] = useState<boolean>(true);
+  const { connection } = useEnvironmentCtx()
+  const { handle } = useWalletIdentity()
+  const [displayName, setDisplayName] = useState<string | undefined>()
+  const [loadingName, setLoadingName] = useState<boolean>(true)
 
   const refreshName = async () => {
     try {
-      setLoadingName(true);
+      setLoadingName(true)
       if (address) {
-        const n = await tryGetName(connection, address);
-        setDisplayName(n);
+        const n = await tryGetName(connection, address)
+        setDisplayName(n)
       }
     } finally {
-      setLoadingName(false);
+      setLoadingName(false)
     }
-  };
+  }
 
   useEffect(() => {
-    void refreshName();
+    void refreshName()
     // handle is desired to be in here to enforce refresh of the name when modal closes
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [connection, address, handle]);
+  }, [connection, address?.toString(), handle])
 
-  return { displayName, loadingName };
-};
+  return { displayName, loadingName }
+}
 
 export const useAddress = (address: PublicKey | undefined) => {
-  const { displayName, loadingName } = useAddressName(address);
+  const { displayName, loadingName } = useAddressName(address)
 
-  if (!address) return <></>;
+  if (!address) return <></>
   return loadingName ? (
     <ContentLoader>
       <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
     </ContentLoader>
   ) : (
-    <div style={{ display: "flex", gap: "5px" }}>
+    <div style={{ display: 'flex', gap: '5px' }}>
       {formatTwitterLink(displayName) || formatShortAddress(address)}
     </div>
-  );
-};
+  )
+}
