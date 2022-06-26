2abfb80177eb98c5fee0529f4697466ef0a3151d
jpbogle <jpbogle22@gmail.com>
2022-06-26 14:12:58 -0400

    Env variable config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    MAINNET_PRIMARY: process.env.MAINNET_PRIMARY,
+    BASE_CLUSTER: process.env.BASE_CLUSTER,
+  },
   webpack: (config) => {
     // Unset client-side javascript that only works server-side
     config.resolve.fallback = { fs: false, module: false, path: false }
