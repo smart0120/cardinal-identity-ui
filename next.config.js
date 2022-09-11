94513c66d73969ad26c38039e3adef72d5c5aa45
jpbogle <jpbogle22@gmail.com>
2022-09-11 12:29:33 -0400

    Skip sentry init if not defined

--- next.config.js
+++ next.config.js
@@ -1,29 +1,31 @@
 const { withSentryConfig } = require('@sentry/nextjs')
 
-module.exports = withSentryConfig(
-  {
-    reactStrictMode: false,
-    env: {
-      MAINNET_PRIMARY: process.env.MAINNET_PRIMARY,
-      BASE_CLUSTER: process.env.BASE_CLUSTER,
-      NEXT_PUBLIC_BASE_URL:
-        process.env.NEXT_PUBLIC_BASE_URL || 'https://identity.cardinal.so',
-    },
-    webpack: (config) => {
-      // Unset client-side javascript that only works server-side
-      config.resolve.fallback = { fs: false, module: false, path: false }
-      return config
-    },
+const nextConfig = {
+  reactStrictMode: false,
+  env: {
+    MAINNET_PRIMARY: process.env.MAINNET_PRIMARY,
+    BASE_CLUSTER: process.env.BASE_CLUSTER,
+    NEXT_PUBLIC_BASE_URL:
+      process.env.NEXT_PUBLIC_BASE_URL || 'https://identity.cardinal.so',
   },
-  {
-    // Additional config options for the Sentry Webpack plugin. Keep in mind that
-    // the following options are set automatically, and overriding them is not
-    // recommended:
-    //   release, url, org, project, authToken, configFile, stripPrefix,
-    //   urlPrefix, include, ignore
+  webpack: (config) => {
+    // Unset client-side javascript that only works server-side
+    config.resolve.fallback = { fs: false, module: false, path: false }
+    return config
+  },
+}
 
-    silent: true, // Suppresses all logs
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options.
-  }
-)
+module.exports =
+  process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
+    ? withSentryConfig(nextConfig, {
+        // Additional config options for the Sentry Webpack plugin. Keep in mind that
+        // the following options are set automatically, and overriding them is not
+        // recommended:
+        //   release, url, org, project, authToken, configFile, stripPrefix,
+        //   urlPrefix, include, ignore
+
+        silent: true, // Suppresses all logs
+        // For all available options, see:
+        // https://github.com/getsentry/sentry-webpack-plugin#options.
+      })
+    : nextConfig
